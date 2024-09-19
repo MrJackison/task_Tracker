f06188448d844ae75d6e938ce30d6d897942caca
Jackson Stumpf <jacksonstumpf3@gmail.com>
2024-09-19 16:07:45 -0300

    Criação da classe task (Unversioned Files)

---
+++ Test_Tracker/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Task task1 = new Task();
+
+        task1.nomearTask();
+        System.out.println(task1.toString());
+    }
+}
